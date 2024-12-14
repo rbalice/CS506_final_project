3aae20a44c68589355a343ebc1ece567334db865
rbalice <123204351+rbalice@users.noreply.github.com>
2024-12-13 20:40:46 -0500

    Update test_notebook.py (the name of file)

--- tests/test_notebook.py
+++ tests/test_notebook.py
@@ -3,7 +3,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 def test_notebook():
     """Test that the Jupyter notebook runs without errors."""
-    with open("your_notebook.ipynb") as f:
+    with open("Final Project Code0.ipynb") as f:
         nb = nbformat.read(f, as_version=4)
 
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
